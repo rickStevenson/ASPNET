0893fc9b707a7bab56b165a5cf63bf8a53e49cf2
RickStevenson <dadsathome1@gmail.com>
2024-10-08 13:34:51 -0600

    Exercise Product2

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -11,11 +11,16 @@ namespace Testing.Controllers
             this.repo = repo;
         }
 
-
         public IActionResult Index()
         {
             var products = repo.GetAllProducts();
             return View(products);
+        }
+
+        public IActionResult ViewProduct(int id)
+        {
+            var product = repo.GetProduct(id);
+            return View(product);
         }
     }
 }
