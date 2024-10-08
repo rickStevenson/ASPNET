034f7d9e99d060f6062c4b874289b47152f0f511
RickStevenson <dadsathome1@gmail.com>
2024-10-08 11:11:35 -0600

    Exercise

---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
+using Testing.Models;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        IEnumerable<Product> IProductRepository.GetAllProducts()
+        {
+            return _conn.Query<Product>("Select * From products");
+        }
+    }
+}
